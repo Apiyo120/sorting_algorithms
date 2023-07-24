397a87ea73b269404c30edffe5e1f889943136f4
JamesMathenge <mathengejames64@gmail.com>
2023-07-24 11:39:06 +0000

    advanced task 102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers using Counting sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ * using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-	int *output, *counting;
-	int max_value, i, j;
+	int *count, *sorted, max, i;
 
 	if (array == NULL || size < 2)
 		return;
 
-	max_value = array[0];
-	for (i = 1; i < (int)size; i++)
-	{
-		if (array[i] > max_value)
-			max_value = array[i];
-	}
-
-	counting = malloc(sizeof(int) * (max_value + 1));
-	if (counting == NULL)
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
 		return;
-	for (i = 0; i <= max_value; i++)
-		counting[i] = 0;
-	for (i = 0; i < (int)size; i++)
-		counting[array[i]]++;
-	for (i = 1; i <= max_value; i++)
-		counting[i] += counting[i - 1];
-
-	output = malloc(sizeof(int) * size);
-	if (output == NULL)
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
 	{
-		free(counting);
+		free(sorted);
 		return;
 	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		j = counting[array[i]] - 1;
-		output[j] = array[i];
-		counting[array[i]]--;
-	}
-	for (i = 0; i < (int)size; i++)
-		array[i] = output[i];
 
-	free(counting);
-	free(output);
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
 }
--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
