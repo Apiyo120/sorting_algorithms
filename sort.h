8f4a151e5a05cd40efe22addcd45b3ee84b08e30
JamesMathenge <mathengejames64@gmail.com>
2023-07-26 09:40:09 +0000

    task 0

--- sort.h
+++ sort.h
@@ -33,6 +33,6 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
-void print_array(const int*array, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif
